e51c2508c2ef3ca8e561809ba2b689f6fb71133b
John Williams <john@thudfactor.com>
2023-10-27 14:50:59 -0400

    Select element

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,46 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
-const Select = ({ label, value, onChange, children }) => {
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+  display: flex;
+  align-items: center;
+  gap: ${24/16}rem;
+  background-color: ${COLORS.transparentGray15};
+  padding: ${12/16}rem ${16/16}rem;
+  border-radius: 8px;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+
+  &:focus-within {
+    outline: 2px solid ${COLORS.primary};
+  }
+`;
+
+const RealSelect = styled.select`
+  position: absolute;
+  inset: 0;
+  border: none;
+  opacity: 0;
+  appearance: none;
+`;
+
+const Select = ({ label, value, onChange, children, ...delegated }) => {
+  const id = "SelectID";
+  console.log(id);
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      {displayedValue} <Icon id="chevron-down" strokeWidth={1} size={24} />
+      <RealSelect {...delegated} value={value} onChange={onChange}>
+        {children}
+      </RealSelect>
+    </Wrapper>
   );
 };
 
